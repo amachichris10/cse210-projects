8db3b1029f7705bc2b96fea7de7276585f4a03b6
amachichris10 <amachi.chris.10@gmail.com>
2026-09-02 18:36:52 -0500

    guess the magic number program added

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string userInput;
+        
+        
+
+        
+        do
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+            int guess = 0;
+            int totalGuesses = 0;
+            do
+            {
+                Console.Write("What is your guess? ");
+                userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                totalGuesses++;
+
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("Congratulations! You guessed the magic number!");
+                    Console.WriteLine($"It took you {totalGuesses} guesses.");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Your guess is too low. Try again!");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too high. Try again!");
+                }
+
+            } while (guess != magicNumber);
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            userInput = Console.ReadLine();
+
+        } while (userInput.ToLower() == "yes");
+
     }
 }
