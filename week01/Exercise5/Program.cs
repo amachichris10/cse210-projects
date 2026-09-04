308eeae2a0abfee35dc6ba59364cfe0b733c32d3
amachichris10 <amachi.chris.10@gmail.com>
2026-09-04 17:18:31 -0500

    get user name and square user's number added

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        WelcomeMessage();
+        string name = UserName();
+        int number = UserNumber();
+        int squared = SquareNumber(number);
+        DisplayResult(name, number, squared);
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("Please enter a number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int number, int squared)
+    {
+        Console.WriteLine($"{name}, the square of {number} is {squared}.");
     }
 }
