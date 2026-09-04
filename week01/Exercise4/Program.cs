13a00bab42b5243a311d8a9e8fa158db50d07b33
amachichris10 <amachi.chris.10@gmail.com>
2026-09-04 17:05:09 -0500

    list of numbers and parsing added

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int sum = 0;
+        float average = 0;
+        int max = 0;
+        int smallestNegative = 0;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+
+                if (number > max)
+                {
+                    max = number;
+                }
+
+                // smallest negative number
+                if (number < 0 && (smallestNegative == 0 || number > smallestNegative))
+                {
+                    smallestNegative = number;
+                }
+
+            }
+            
+        } while (number != 0);
+        
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        average = (float)sum / numbers.Count;
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Max: " + max);
+        Console.WriteLine("Smallest Negative: " + smallestNegative);
     }
 }
